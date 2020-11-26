d872e1fb415515a7f6e21becfb4da35a702f9328
Michal Kuncl <michal.kuncl@gmail.com>
2020-11-26 21:13:58 +0100

    move zypper update count to /var/log

--- checkZypper.sh
+++ checkZypper.sh
@@ -2,4 +2,4 @@
 # Please place me to cron.hourly
 zypper ref
 myresult=`zypper list-updates |wc -l|bc -l`
-echo $myresult - 3|bc -l > /tmp/pkg_update_totals
+echo $myresult - 3|bc -l > /var/log/pkg_update_totals
